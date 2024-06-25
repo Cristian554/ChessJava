e7b8b2ed03517d06915e859ae2654d319ec1500a
Cristian554 <cristianpinto256@gmail.com>
2024-06-25 14:59:10 -0300

    Special Move Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,7 +41,13 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece); // adiciona a peça capturada
 				}
-			} catch (ChessException e) {
+				if(chessMatch.getPromoted() != null) {					
+					System.out.print("Enter piece for Promotion (B/N/R/Q) :"); // escolhendo a peça perguntando para o Usuário
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+			} 
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			} catch (InputMismatchException e) {
